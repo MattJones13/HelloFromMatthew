5d52f32234c0fe2de57976e1e7ab298ada81668d
MattJones13 <MatthewKennethJones@gmail.com>
2021-01-30 18:48:36 -0700

    change

--- src/prove/Hello.java
+++ src/prove/Hello.java
@@ -3,5 +3,6 @@ package prove;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Matthew.");
+        System.out.println("Hello Matthew.");
     }
 }
