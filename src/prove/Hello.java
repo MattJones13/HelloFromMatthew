c3b617ae03f8cea18cbd22283f50ff3735b2c838
jalexander-byu <ale20006@byui.edu>
2021-01-30 21:00:41 -0700

    Hello from Jacob

--- src/prove/Hello.java
+++ src/prove/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Matthew.");
         System.out.println("Hello Matthew.");
+        System.out.println("Hi Matthew, my name is Jacob.");
     }
 }
